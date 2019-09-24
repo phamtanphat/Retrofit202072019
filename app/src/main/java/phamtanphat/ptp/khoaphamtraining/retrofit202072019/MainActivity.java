aaf4f2f3f092cf687a26a6e3fdfc650c069e83b7
phatdroid94 <phatdroid94@gmail.com>
2019-09-24 19:26:28 +0700

    a

--- app/src/main/java/phamtanphat/ptp/khoaphamtraining/retrofit202072019/MainActivity.java
+++ app/src/main/java/phamtanphat/ptp/khoaphamtraining/retrofit202072019/MainActivity.java
@@ -64,5 +64,6 @@ public class MainActivity extends AppCompatActivity {
 
             }
         });
+//        http://api.openweathermap.org/data/2.5/weather?appid=86183a23377ed034aef7aad102f43d64&units=metric&q=saigon
     }
 }
